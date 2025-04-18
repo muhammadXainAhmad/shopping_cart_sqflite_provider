160394ab148901d71308188dbae2b34793e69710
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-04-19 01:13:56 +0500

    Created insertItem function

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
-
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shopping_cart_sqflite_provider/cart_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DbHelper {
@@ -26,6 +26,12 @@ class DbHelper {
           'CREATE TABLE cart (id INTEGER PRIMARY KEY , productId VARCHAR UNIQUE,productName TEXT,initialPrice INTEGER, productPrice INTEGER , quantity INTEGER, unitTag TEXT , image TEXT )',
         );
       },
-    ); 
+    );
+  }
+
+  Future<Cart> insertItem(Cart cart) async {
+    var dbClient = await getDb();
+    await dbClient!.insert("cart", cart.toMap());
+    return cart;
   }
 }
