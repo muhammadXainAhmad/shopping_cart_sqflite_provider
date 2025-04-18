f85ba8c4241c91b4f401d7fa48caf7913f9579eb
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-04-18 23:51:56 +0500

    Display product list

---
+++ lib/constants.dart
@@ -0,0 +1,20 @@
+List<String> productImage = [
+  "https://www.shutterstock.com/shutterstock/photos/2327230131/display_1500/stock-photo-isolated-mango-fresh-organic-mango-with-leaves-isolated-clipping-path-mango-macro-studio-photo-2327230131.jpg",
+  "https://www.shutterstock.com/shutterstock/photos/2053015835/display_1500/stock-photo-orange-with-sliced-and-green-leaves-isolated-on-white-background-2053015835.jpg",
+  "https://www.shutterstock.com/shutterstock/photos/2490657677/display_1500/stock-photo-fresh-green-grape-cluster-isolated-on-white-background-2490657677.jpg",
+  "https://www.shutterstock.com/shutterstock/photos/2474380581/display_1500/stock-photo-banana-bunch-banana-fruit-clipping-path-banana-isolated-on-white-background-banana-macro-studio-2474380581.jpg",
+  "https://www.shutterstock.com/shutterstock/photos/2519733627/display_1500/stock-photo-cherry-with-stem-and-leaves-isolated-on-white-background-2519733627.jpg",
+  "https://www.shutterstock.com/shutterstock/photos/2492302693/display_1500/stock-photo-peach-isolated-on-white-background-one-whole-peach-fruits-food-for-vegetarian-and-vegan-diets-2492302693.jpg",
+  "https://www.shutterstock.com/shutterstock/photos/2509426595/display_1500/stock-photo-mix-berries-in-closeup-on-white-background-2509426595.jpg",
+];
+List<String> productName = [
+  "Mango",
+  "Orange",
+  "Grapes",
+  "Banana",
+  "Cherry",
+  "Peach",
+  "Mixed Berries",
+];
+List<String> productUnit = ["KG", "Dozen", "KG", "Dozen", "KG", "KG", "KG"];
+List<int> productPrice = [10, 20, 30, 40, 50, 60, 70];
--- lib/product_list.dart
+++ lib/product_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_cart_sqflite_provider/constants.dart';
 
 class ProductListPage extends StatefulWidget {
   const ProductListPage({super.key});
@@ -12,8 +13,75 @@ class _ProductListPageState extends State<ProductListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Product List"),
-        actions: [Icon(Icons.shopping_bag_rounded)],
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        title: Text("Product List", style: TextStyle(color: Colors.white)),
+        actions: [
+          Badge(
+            label: Text("0"),
+            child: Icon(
+              Icons.shopping_bag_outlined,
+              color: Colors.white,
+              size: 28,
+            ),
+          ),
+
+          SizedBox(width: 20),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: productName.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Image(
+                                width: 100,
+                                height: 100,
+                                image: NetworkImage(
+                                  productImage[index].toString(),
+                                ),
+                              ),
+                              SizedBox(width: 30),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    productName[index],
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    '${productUnit[index]} \$${productPrice[index]}',
+                                  ),
+                                ],
+                              ),
+                              Spacer(),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text("Add to cart"),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
