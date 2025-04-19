252213287be7620be957841cb08ca88198b5b9dc
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-04-20 01:58:31 +0500

    Integrated cartProvider on materialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_cart_sqflite_provider/cart_provider.dart';
 import 'package:shopping_cart_sqflite_provider/product_list.dart';
 
 void main() {
@@ -9,13 +11,16 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return ChangeNotifierProvider(
+      create: (_) => CartProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: const ProductListPage(),
       ),
-      home: ProductListPage(),
     );
   }
 }
